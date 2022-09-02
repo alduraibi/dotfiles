695fbc349d0ef718acf582fd76ffd7a651382ed0
Ghassan Alduraibi <git@ghassan.dev>
2022-09-02 07:42:33 +0300

    update stowd cfg to v0.2.0

--- neovim/.config/nvim/lua/plugins/lspconfig/init.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig/init.lua
@@ -1,14 +1,29 @@
 -- plugins/lspconfig.lua
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = {
+  require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+}
 
 require('lspconfig').bashls.setup({ capabilities = capabilities }) -- bash
-require('lspconfig').pylsp.setup({ capabilities = capabilities }) -- python
+require('lspconfig').pylsp.setup({
+  capabilities = capabilities,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          maxLineLength = 88,
+        },
+      },
+    },
+  },
+}) -- python
 require('lspconfig').yamlls.setup({ capabilities = capabilities }) -- yaml
 require('lspconfig').jsonls.setup({ capabilities = capabilities }) -- json
 
 -- termux: `pkg install lua-language-server`
-require('lspconfig').sumneko_lua.setup({ capabilities = capabilities }) -- lua
+require('lspconfig').sumneko_lua.setup({
+  capabilities = capabilities,
+}) -- lua
 
 -- LSPs that do not work on termux
 if not vim.fn.has('termux') == 1 then
